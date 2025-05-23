0192db3e82e47ec8f5a902869946a21fdf0000e2
Bernst4315 <bernst4315@gmail.com>
2025-05-23 13:32:58 -0600

    added more notes

--- notes.ts
+++ notes.ts
@@ -16,13 +16,25 @@ let favoritFood: Food = "pizza" // equvalent to let favoritFood: string = "pizza
 
 //custom types objs
 
+type Address = {
+        street: string
+        city: string
+        country: string 
+}
+
 type Person = { //like a class, or even a model in mongoose
     name: string //assigns val type to each prop, "," optional could use ";"
     age: number
     isStudent: boolean
+    address?: Address //"?" makes address property optional, but reduces type safety
+    // address: { //can have nested obj 
+    //     street: string
+    //     city: string
+    //     country: string //common to make a separate type
+    // }
 }
 
-let person1: Person = {
+let person1: Person = { //ts shows error, for it lack the address obj
     name: "Joe",
     age: 42,
     isStudent: true
